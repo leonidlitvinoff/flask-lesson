398e879fb74132c30cf3d5a41534424239928a40
Леонид Литвинов <leonidlitvinoff@yandex.ru>
2020-03-06 22:22:18 +0700

    Task seven

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from flask import Flask, render_template, url_for, redirect
 app = Flask(__name__)
-COMMAND = ['Артем',
-           'Максим Кадулин',
-           'Сергей Смирнов',
-           'Андрей Васильков']
 
 
-@app.route('/distribution')
-def distribution():
-    return render_template('distribution.html', title='Каюты', data=COMMAND)
+@app.route('/table/<sex>/<int:age>')
+def table(sex, age):
+    config = {'title': 'Каюты'}
+    if sex == 'female' and age >= 21:
+        config['color'] = url_for('static', filename='img/female_grand.jpg')
+        config['person'] = url_for('static', filename='img/big.jpg')
+    elif sex == 'female' and age < 21:
+        config['color'] = url_for('static', filename='img/female_small.jpg')
+        config['person'] = url_for('static', filename='img/small.jpg')
+    elif sex == 'male' and age < 21:
+        config['color'] = url_for('static', filename='img/male_small.jpg')
+        config['person'] = url_for('static', filename='img/small.jpg')
+    else:
+        config['color'] = url_for('static', filename='img/male_grand.jpg')
+        config['person'] = url_for('static', filename='img/big.jpg')
+    return render_template('table.html', **config)
 
 
 if __name__ == '__main__':
