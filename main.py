a22f13cd38000326eea7ffb757ec66475e7fcd88
Леонид Литвинов <leonidlitvinoff@yandex.ru>
2020-03-06 20:08:34 +0700

    Task four

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
-@app.route('/list_prof/<type>')
-def list_prof(type):
-    config = {'data': ['Инженер-исследователь',
-                       'Пилот',
-                       'Строитель',
-                       'Экзобиолог',
-                       'Врач',
-                       'Климатолог']}
-    if type != 'ol' and type != 'ul':
-        config['type'] = 'Неверный тип'
-        return 'Error'
-    config['type'] = type
-    return render_template('list_prof.html', **config)
+@app.route('/answer')
+@app.route('/auto_answer')
+def answer():
+    config = {'title': 'Анкета',
+              'surname': 'Беркут',
+              'name': 'Крылов',
+              'education': '3 Высшее',
+              'profession': 'Экзобиолог',
+              'sex': 'Мужской',
+              'motivation': 'Отсутствие мотивации, тревожность и конфликтность.',
+              'ready': 'Да',
+              'css_url': url_for('static', filename='css/style.css')}
+    return render_template('auto_answer.html', **config)
 
 
 if __name__ == '__main__':
